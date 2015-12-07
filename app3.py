685f5c8839f3e6a1f12986e0fe47563f8fa10a34
Junhui <junhui@umich.edu>
2015-12-06 21:53:03 -0500

    12.6

--- app3.py
+++ app3.py
@@ -25,20 +25,24 @@ def default():
     return render_template("index.html", name = "index", title = "HOME PAGE")			#The argument should be in templates folder
 
 @app.route('/index')          								#This is the main URL
-def home():
+def index():
     return render_template("index.html", name = "index", title = "HOME PAGE")			#The argument should be in templates folder
 
-# @app.route('/courses')          								#This is the main URL
-# def courses():
-#     return render_template("courses.html", name = "courses", title = "COURSES PAGE")			#The argument should be in templates folder
-#
-# @app.route('/interests')          								#This is the main URL
-# def interests():
-#     return render_template("interests.html", name = "interests", title = "INTERESTS PAGE")			#The argument should be in templates folder
-#
-# @app.route('/other')          								#This is the main URL
-# def other():
-#     return render_template("other.html", name = "other", title = "OTHER PAGE")			#The argument should be in templates folder
+@app.route('/mypage')          								#This is the main URL
+def community():
+    return render_template("mypage.html", name = "mypage", title = "MYPAGE PAGE")			#The argument should be in templates folder
+
+@app.route('/history')          								#This is the main URL
+def history():
+    return render_template("history.html", name = "history", title = "HISTORY PAGE")			#The argument should be in templates folder
+
+@app.route('/tools')          								#This is the main URL
+def tools():
+    return render_template("tools.html", name = "tools", title = "TOOLS PAGE")			#The argument should be in templates folder
+
+@app.route('/masterpieces')          								#This is the main URL
+def masterpieces():
+    return render_template("masterpieces.html", name = "masterpieces", title = "MASTERPIECES PAGE")
 
 @app.errorhandler(404)
 def pageNotFound(e):
@@ -48,8 +52,8 @@ def pageNotFound(e):
 def pageNotFound(e):
     return render_template('500.html'), 500
 
-@app.route('/contact', methods=['GET', 'POST'])
-def contact():
+@app.route('/register', methods=['GET', 'POST'])
+def register():
     form = ContactForm()
 
     if request.method == 'POST':
@@ -62,44 +66,29 @@ def contact():
         cursor = conn.cursor()
         cursor.execute("INSERT INTO user (userName,password) VALUES ('{0}', '{1}')".format(newusername,newpassword))
         conn.commit()
-        return 'Form posted.'
+        return 'Congratulations, you have successfully registered!'
 
     elif request.method == 'GET':
-        return render_template('contact.html', form=form)
-# def Authenticate():
-#     form = ContactForm()
-#     if request.method == 'POST':
-#         username = form.name.data
-#         password = form.password.data
-#         cursor = mysql.connect().cursor()
-#         cursor.execute("SELECT * from User where Username='" + username + "' and Password='" + password + "'")
-#         data = cursor.fetchone()
-#         if data is None:
-#             return "Username or Password is wrong"
-#         else:
-#             return render_template('contact.html', data=data, form=form)
-#
-#     elif request.method == 'GET':
-#         return render_template('contact.html', form=form)
+        return render_template('register.html', form=form)
 
-# @app.route('/login', methods=['GET', 'POST'])
-# def contact():
-#     form = ContactForm()
-#
-#     if request.method == 'POST':
-#         newusername = form.name.data
-#         newpassword = form.password.data
-#         print form.name.data
-#         print form.password.data
-#         # cursor = mysql.connect().cursor()
-#         conn = mysql.connect()
-#         cursor = conn.cursor()
-#         cursor.execute("INSERT INTO user (userName,password) VALUES ('{0}', '{1}')".format(newusername,newpassword))
-#         conn.commit()
-#         return 'Form posted.'
-#
-#     elif request.method == 'GET':
-#         return render_template('login.html', form=form)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = ContactForm()
+
+    if request.method == 'POST':
+        username = form.name.data
+        password = form.password.data
+        cursor = mysql.connect().cursor()
+        cursor.execute("SELECT * from User where Username='" + username + "' and Password='" + password + "'")
+        data = cursor.fetchone()
+        if data is None:
+            return "Username or Password is wrong"
+        else:
+            return render_template('login.html', data=data, form=form)
+
+    elif request.method == 'GET':
+        return render_template('login.html', form=form)
+
 
 
 if __name__ == '__main__':
