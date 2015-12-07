0b2a382c0052cc3a984384fd362e7ab025e0d5f8
Junhui <junhui@umich.edu>
2015-12-07 00:22:43 -0500

    12.7

--- app3.py
+++ app3.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request  #NEW IMPORT -- request
+from flask import Flask, render_template, request,url_for ,flash #NEW IMPORT -- request
 from flask.ext.wtf import Form
 from wtforms import TextField, TextAreaField, SubmitField
 from forms import ContactForm 					# NEW IMPORT LINE
 from flaskext.mysql import MySQL
+from flask.ext.login import login_user, login_required, logout_user, LoginManager
 
 mysql = MySQL()
 app = Flask(__name__)
@@ -13,11 +14,11 @@ app.config['MYSQL_DATABASE_DB'] = 'EmpData'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
+login_manager = LoginManager()  #don't understand
+login_manager.init_app(app)     #don't understand
+login_manager.login_view = "user.login"
 
 
-# app = Flask(__name__)    #This is creating a new Flask object
-
-# app.secret_key = 'WebDesign'
 
 #decorator that links...
 @app.route('/')          								#This is the main URL
@@ -52,6 +53,10 @@ def pageNotFound(e):
 def pageNotFound(e):
     return render_template('500.html'), 500
 
+@login_manager.user_loader    #don't understand
+def load_user(user_id):
+    return User.get(user_id)
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = ContactForm()
@@ -80,15 +85,23 @@ def login():
         password = form.password.data
         cursor = mysql.connect().cursor()
         cursor.execute("SELECT * from User where Username='" + username + "' and Password='" + password + "'")
-        data = cursor.fetchone()
+        data = cursor.fetchone()   # define after this ?
         if data is None:
             return "Username or Password is wrong"
         else:
+            login_user(user) #user is not defined, where the hell to define it?
+            flash('Logged in successfully')
             return render_template('login.html', data=data, form=form)
 
     elif request.method == 'GET':
         return render_template('login.html', form=form)
 
+@app.route('/logout')
+@login_required         # means after user login?
+def logout():
+    session.pop('logged_in',None)
+    flash('You were logged out.')
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
